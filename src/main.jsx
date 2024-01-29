263e1ec5c88f22e59d70848e74a53bbb12bb625f
SAYAN <sayanhumanist@gmail.com>
2024-01-29 21:17:41 +0530

    varcel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Provider } from 'react-redux'
 import store from './store/store.js'
@@ -71,6 +72,7 @@ const rout= createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={rout}/>
+    <SpeedInsights/>
 
   </Provider>,
 )
